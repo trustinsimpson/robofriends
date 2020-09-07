ff7fc87ef9e41c7d707579312fe94199977ab1af
Trustin Simpson <contact@trustinsimpson.com>
2020-09-07 18:27:13 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
+import './App.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
